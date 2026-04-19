ef851d14ecef47f4c1d77fb87bdb5caff8aef2b4
shar-das <125547303+shar-das@users.noreply.github.com>
2026-04-19 13:39:44 +0530

    minor change

--- src/main/java/listeners/RetryTransformer.java
+++ src/main/java/listeners/RetryTransformer.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
-public class AnnotationTransformer implements IAnnotationTransformer{
+public class RetryTransformer implements IAnnotationTransformer{
 	
 	@Override
 	public void transform(ITestAnnotation annotation,
