f0c58682fefd00593b6504241f7f38e84e236a67
shar-das <125547303+shar-das@users.noreply.github.com>
2026-04-19 13:03:49 +0530

    added ThreadLocal for driver

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,18 +1,17 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import utils.ConfigFileReader;
+import utils.DriverFactory;
 
 public class BaseTest {
 
     public WebDriver driver;
+    protected DriverFactory driverFactory;
     protected String email;
     protected String password;
     String browser, baseUrl;
@@ -26,25 +25,8 @@ public class BaseTest {
         email = ConfigFileReader.getProperty("email");
         password = ConfigFileReader.getProperty("password");
 
-        switch(browser.toLowerCase()) {
-
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                if(isHeadless) chromeOptions.addArguments("--headless=new");
-                chromeOptions.addArguments("--window-size=1920,1080");
-                driver = new ChromeDriver(chromeOptions);
-                break;
-
-            case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                if(isHeadless) edgeOptions.addArguments("--headless=new");
-                edgeOptions.addArguments("--window-size=1920,1080");
-                driver = new EdgeDriver(edgeOptions);
-                break;
-
-            default:
-                throw new RuntimeException("Browser not supported: " + browser);
-        }
+        driverFactory = new DriverFactory();
+        driver = driverFactory.initDriver(browser, isHeadless);
 
         context.setAttribute("driver", driver);
         driver.get(baseUrl);
